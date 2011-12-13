0042ee2f4482509e9f8d28ca2ce5217670940528
Kenneth Sohrmann <sohrmann88@hotmail.com>
2011-12-14 00:01:17 +0100

    Didn't get committed at previous commit.

---
+++ BDSAE2011_NemID_Project/BDSA_Project_Cryptography/StringData.cs
@@ -0,0 +1,18 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="StringData.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace BDSA_Project_Communication
+{
+
+    public class StringData
+    {
+        public static string AuthUri = "http://localhost:8081/";
+
+        public static string ThirdUri = "http://localhost:8082/";
+
+        public static string filePath = @"C:\Users\Kenneth88\BDSAE2011_NemID_Project\BDSAE2011_NemID_Project\";
+    }
+}
